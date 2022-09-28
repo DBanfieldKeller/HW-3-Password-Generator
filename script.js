20dd6280fb0147242d5138f155ef1efbed7d604e
Goat <dpajbk@gmail.com>
2022-09-28 17:21:37 -0400

    Added basic password criteria variables and prompts/confirms

--- script.js
+++ script.js
@@ -2,10 +2,21 @@
 var generateBtn = document.querySelector("#generate");
 
 // Generate random seed
-var randomNumber =Math.random();
+var randomSeed =Math.floor(Math.random() * 100);
 
-console.log(randomNumber);
+console.log(randomSeed);
 
+// Prompt password criteria
+
+var digitNumber = prompt("Please indicate the number of digits for your password (8-128)", "8");
+var specialCharacters = confirm("Would you like to use special characters?\nOK=Yes Cancel=No");
+var uppercase = confirm("Would you like to use uppercase letters in addition to lowercase?\nOK=Yes Cancel=NO");
+var numbers = confirm("Would you like to use numbers?\nOK=Yes Cancel=No");
+
+
+console.log(digitNumber);
+console.log(specialCharacters);
+console.log(uppercase);
 
 
 // Write password to the #password input
